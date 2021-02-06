0a5ab34f0cce52c6843d6cba7f4b45320ae3c12e
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-02-07 03:14:45 +0500

    react plus flask template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [currentTime, setCurrentTime] = React.useState(0);
+
+
+
+
+  React.useEffect(
+    () => {
+      fetch('/time')
+      .then(res => res.json())
+      .then( data => {
+        setCurrentTime(data.time)
+      })
+    },
+    []
+  )
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           {Date(currentTime)}
         </p>
         <a
           className="App-link"
@@ -19,7 +38,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
